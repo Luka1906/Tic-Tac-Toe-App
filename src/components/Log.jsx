d8ae8855baf9385f7933190f6e3e6b48f12b4cec
Luka Matovic <lukamatovic1994@gmail.com>
2025-03-07 16:58:08 -0600

    Added winning combinations and game over functionality

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,10 +1,9 @@
 export default function Log({turns}) {
     return (
         <ol id="log">
-            {turns.map((turn, index) => (
-                <li key={index}>
-                    <span className="player-name">{turn.player}</span>
-                    <span className="player-symbol">{turn.square.row}, {turn.square.col}</span>
+            {turns.map((turn) => (
+                <li key={`${turn.square.row}${turn.square.col}`}>
+                    {turn.player} selected {turn.square.row}, {turn.square.col}
                 </li>
             ))}
         </ol>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,30 +1,39 @@
 import { useState } from "react";
 
-export default function Player({initialName, symbol, isActive}) {
-    const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
-    
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditing() {
-        setIsEditing((editing) => !editing);
-    };
-
-    function handleChange(event) {
-        setPlayerName(event.target.value);
+  function handleEditing() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
     }
+  }
 
-    return (
-        <li className={isActive ? "active" : undefined}>
-        <span className="player">
-            {isEditing ? (
-            <input onChange={handleChange} type="text" value={playerName}  />
-            ) : (
-            <span className="player-name">{playerName}</span>
-            )}
- 
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button onClick={handleEditing}>{isEditing? "Save" : "Edit"}</button>
-      </li>
-    )
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+    
+   
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input onChange={handleChange} type="text" value={playerName} />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditing}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
